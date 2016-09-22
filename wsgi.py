84f16094f0ad2e29bec719ef080a9c39acb26938
John Flynn Matthew <john@compunique.com>
2016-09-22 12:52:35 -0700

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ from bottle import route, run, default_app
 
 @route('/')
 def index():
-    return "<h1> hello OpenShift V3 Ninja, Kyle</h1>"
+    return "<h1> hello OpenShift V3 Ninja</h1>"
 
 if __name__ == '__main__':
     run(host='0.0.0.0', port=8080)
